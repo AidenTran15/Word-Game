b5c22f74a904a9418e65b15ff2ee4f4c69203b9f
AidenTran15 <aidenkiettran@gmail.com>
2024-08-09 16:44:23 +1000

    Added link in navigation bar for NextWord and Synonym-Finder

--- word-game/src/components/Navbar/Navbar.js
+++ word-game/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import WordGameHeader from '../../assets/3.png'; // Adjust the path based on your file structure
 
@@ -6,6 +7,10 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <img src={WordGameHeader} alt="Word Game" className="header-image" />
+            <div className="nav-links">
+                <Link to="/game" className="nav-link">NextWord</Link>
+                <Link to="/synonym-finder" className="nav-link">Synonym-Finder</Link>
+            </div>
         </nav>
     );
 };
