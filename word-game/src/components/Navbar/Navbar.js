b3519b1854a573d4178bfa53fa5b2132a21aa8c1
AidenTran15 <aidenkiettran@gmail.com>
2024-08-11 19:23:21 +1000

    Added dropdown menu to Navbar with "More Game" button to show "Next Word" and "Synonym Finder"

--- word-game/src/components/Navbar/Navbar.js
+++ word-game/src/components/Navbar/Navbar.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import WordGameHeader from '../../assets/3.png'; // Adjust the path based on your file structure
 
 const Navbar = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
     return (
         <nav className="navbar">
             <img src={WordGameHeader} alt="Word Game" className="header-image" />
             <div className="nav-links">
-                <Link to="/game" className="nav-link">Next Word</Link>
-                <Link to="/synonym-finder" className="nav-link">Synonym Finder</Link>
+                <div className="dropdown">
+                    <button className="dropdown-button" onClick={toggleDropdown}>
+                        More Game
+                    </button>
+                    {dropdownOpen && (
+                        <div className="dropdown-content">
+                            <Link to="/game" className="dropdown-item">Next Word</Link>
+                            <Link to="/synonym-finder" className="dropdown-item">Synonym Finder</Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </nav>
     );
