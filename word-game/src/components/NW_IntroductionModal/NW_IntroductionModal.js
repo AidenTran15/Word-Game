7ee91f316ba58818c7eb767c5f2ef769a8763135
AidenTran15 <aidenkiettran@gmail.com>
2024-08-11 22:13:04 +1000

    Apply "Playfair Display" font to modal heading

--- word-game/src/components/NW_IntroductionModal/NW_IntroductionModal.js
+++ word-game/src/components/NW_IntroductionModal/NW_IntroductionModal.js
@@ -5,7 +5,7 @@ const NW_IntroductionModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>Welcome to the Next Word Game!</h2>
+        <h2>Welcome to Next Word Game!</h2>
         <p>
           This is a fun game where you and the computer take turns to enter words.
           The next word must start with the last letter of the previous word. Try not to use the same word twice!
--- word-game/src/components/SF_IntroductionModal/SF_IntroductionModal.js
+++ word-game/src/components/SF_IntroductionModal/SF_IntroductionModal.js
@@ -5,7 +5,7 @@ const IntroductionModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>Welcome to the Synonym Finder Game!</h2>
+        <h2>Welcome to Synonym Finder Game!</h2>
         <p>
           In this game, you'll be given a word, and your goal is to choose the correct synonym from the options provided. 
           You'll have 45 seconds to answer as many questions as you can. Each correct answer will earn you a point.
